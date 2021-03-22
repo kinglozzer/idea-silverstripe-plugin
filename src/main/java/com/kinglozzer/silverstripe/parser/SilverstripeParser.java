0cf6121b7a427fa19fc553afd2a21e6cbd4d597f
Loz Calver <lozcalver@bigfork.co.uk>
2021-03-22 17:59:04 +0000

    Tidy unused import

--- src/main/java/com/kinglozzer/silverstripe/parser/SilverstripeParser.java
+++ src/main/java/com/kinglozzer/silverstripe/parser/SilverstripeParser.java
@@ -4,7 +4,6 @@ import com.intellij.lang.ASTNode;
 import com.intellij.lang.PsiBuilder;
 import com.intellij.lang.PsiBuilder.Marker;
 import com.intellij.lang.PsiParser;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.Pair;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.tree.TokenSet;
